19cecd43e4ec160b4b30a01c5db1054c040ab343
Khen Vonoe Gabriel <dalanginkhen@gmail.com>
2026-06-16 21:28:01 +0800

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Features from "../components/Features"; // Tiyaking may s sa dulo (Features)
 import Pricing from "../components/Pricing";   // Naka-link na sa bagong Rates Matrix natin
-import CTA from "../components/CTA";
+import CTA from "../components/ContactSecton";
 function Home() {
     return (
         <div className="min-h-screen bg-black text-white font-sans antialiased selection:bg-yellow-400 selection:text-black">
