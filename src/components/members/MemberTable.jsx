d3cadc5f93444b48ff30482018cfd7cd3a84b101
Khen Vonoe Gabriel <dalanginkhen@gmail.com>
2026-06-20 12:37:42 +0800

    isapa

--- src/components/members/MemberTable.jsx
+++ src/components/members/MemberTable.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SortDropdown from './SortDropdown';
+import SortDropdown from '..src/dashboard/SortDropdown';
 import { Search, CheckCircle, XCircle, RefreshCw } from 'lucide-react';
 
 export default function MemberTable({ 
